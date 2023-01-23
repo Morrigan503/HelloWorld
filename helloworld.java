af0b29ebdad4df6b9d759ab5f189cb151f1d6b5c
Gabriel Swanson <gabriel.swanson@pcc.edu>
2023-01-23 12:33:28 -0800

    added a date function

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,7 @@
+import java.util.Date;
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 }
